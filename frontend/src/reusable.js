ba62c085fcc52799bef5573caf87eec5ab6219c2
srk0102 <ramakrishnasiva128@gmail.com>
2022-06-22 19:56:18 +0530

    your commit

--- frontend/src/reusable.js
+++
@@ -1,4 +0,0 @@
-export * from "./components/reusable/topBar"
-export * from "./components/reusable/sideBar"
-export * from "./components/reusable/icon"
-export * from "./components/reusable/cardBox"
--- frontend/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {countReducer} from "../store/reducer/counter"
-
-export default configureStore ({reducer:{
-  Sample : countReducer
-}})
-
